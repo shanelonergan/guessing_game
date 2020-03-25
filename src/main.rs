584314b7ce43a983fab1407e93cc4d14551d03f3
shanelonergan <sptlonergan@gmail.com>
2020-03-25 15:27:47 -0400

    added loop

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
     let mut guess = String::new();
 
@@ -16,13 +17,14 @@ fn main() {
         .expect("Failed to read line");
 
     let guess: u32 = guess.trim().parse()
-        .expect("Please Type a Number!")
+        .expect("Please Type a Number!");
 
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!")
-        Ordering::Greater => println!("Too big!")
-        Ordering::Equal => println!("💥 You Win 💥")
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("💥 You Win 💥"),
+    }
     }
 }
