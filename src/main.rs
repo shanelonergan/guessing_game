4260c12eaf7b228602c582a22fa830a795622e84
shanelonergan <sptlonergan@gmail.com>
2020-03-25 15:20:41 -0400

    convert guess string to u32 number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -14,5 +15,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please Type a Number!")
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!")
+        Ordering::Greater => println!("Too big!")
+        Ordering::Equal => println!("💥 You Win 💥")
+    }
 }
